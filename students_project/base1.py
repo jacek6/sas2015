a1821a7644405646fa65f7ea4d7a44d9153734f7
Jacek Skoczylas <gjacek6@gmail.com>
2016-03-21 18:41:21 +0100

    created file to be importing in most small-programs

--- students_project/base1.py
+++ students_project/base1.py
@@ -42,21 +42,3 @@ def sentiment_classification(X, y, n_folds=10, classifier=None):
             results['cm'].append(metrics.confusion_matrix(y_test, predicted))
 
         return results
-
-
-class MyCls:
-    def fit(self, X, y):
-        self.yy = y
-        return self
-    
-    def predict(self, X):
-        result = np.zeros(shape=(X.shape[0]))
-        for i in range(0, result.size-1):
-            result[i] = 1
-        return result
-        
-print ""
-#results = sentiment_classification(X, y, n_folds=4, classifier=LogisticRegression())
-results = sentiment_classification(X, y, n_folds=4, classifier=MyCls())
-print 'Accuracy: %s' % np.mean(results['acc'])
-print 'F1-measure: %s' % np.mean(results['f1'])
---
+++ students_project/run2.py
@@ -0,0 +1,17 @@
+import base1
+
+class MyCls:
+    def fit(self, X, y):
+        self.yy = y
+        return self
+    
+    def predict(self, X):
+        result = np.zeros(shape=(X.shape[0]))
+        for i in range(0, result.size-1):
+            result[i] = 1
+        return result
+        
+#results = sentiment_classification(X, y, n_folds=4, classifier=LogisticRegression())
+results = sentiment_classification(X, y, n_folds=4, classifier=MyCls())
+print 'Accuracy: %s' % np.mean(results['acc'])
+print 'F1-measure: %s' % np.mean(results['f1'])
