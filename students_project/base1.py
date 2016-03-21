228edefed52818f1abe547fff2f697861ccd77a5
Jacek Skoczylas <gjacek6@gmail.com>
2016-03-21 18:59:32 +0100

    base1 code improvements

--- students_project/base1.py
+++ students_project/base1.py
@@ -2,7 +2,7 @@ import pandas as pd
 from os import path
 import numpy as np
 
-notebook_path = 'C:\Users\Jacek\Documents/mgr_sem1/zmiatd/lab/begins/sas2015-master/sas2015-master'
+notebook_path = '../'
 data = pd.read_csv(path.join(notebook_path, 'data', 'SemEval-2014.csv'), index_col=0)
 docs = data['document'] 
 y = data['sentiment'] # standart name for labels/classes variable 
@@ -12,7 +12,7 @@ X = count_vect.fit_transform(docs)
 from sklearn import metrics, cross_validation
 from sklearn.linear_model import LogisticRegression
 
-def sentiment_classification(X, y, n_folds=10, classifier=None):
+def sentiment_classification(X, y, n_folds=10, classifier=None, printing=False):
         """
         Counting sentiment with cross validation - supervised method
         :type X: ndarray feature matrix for classification
@@ -24,15 +24,15 @@ def sentiment_classification(X, y, n_folds=10, classifier=None):
         results = {'acc': [], 'prec': [], 'rec': [], 'f1': [], 'cm': []}
         kf = cross_validation.StratifiedKFold(y, n_folds=n_folds, shuffle=True)
         for train_index, test_index in kf:
-            print "train_index = %s, test_index %s" % (train_index.size, test_index.size)
+            if printing: print "train_index = %s, test_index %s" % (train_index.size, test_index.size)
             X_train, X_test = X[train_index], X[test_index]
             y_train, y_test = y[train_index], y[test_index]
 
             ######################## Most important part ########################## 
-            print 'X_train = %s, y_train = %s' % (X_train.shape, y_train.shape)
+            if printing: print 'X_train = %s, y_train = %s' % (X_train.shape, y_train.shape)
             clf = classifier.fit(X_train, y_train) # train the classifier 
             predicted = clf.predict(X_test) # predict test the classifier 
-            print 'X_test = %s, predicted = %s' % (X_test.shape, predicted.shape)
+            if printing: print 'X_test = %s, predicted = %s' % (X_test.shape, predicted.shape)
             #######################################################################
 
             results['acc'].append(metrics.accuracy_score(y_test, predicted))
--- students_project/run2.py
+++ students_project/run2.py
@@ -1,4 +1,4 @@
-import base1
+from base1 import *
 
 class MyCls:
     def fit(self, X, y):
@@ -8,10 +8,10 @@ class MyCls:
     def predict(self, X):
         result = np.zeros(shape=(X.shape[0]))
         for i in range(0, result.size-1):
-            result[i] = 1
+            result[i] = 2
         return result
         
 #results = sentiment_classification(X, y, n_folds=4, classifier=LogisticRegression())
-results = sentiment_classification(X, y, n_folds=4, classifier=MyCls())
+results = sentiment_classification(X, y, n_folds=4, classifier=MyCls(), printing=False)
 print 'Accuracy: %s' % np.mean(results['acc'])
 print 'F1-measure: %s' % np.mean(results['f1'])
