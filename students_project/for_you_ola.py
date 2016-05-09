7698b6beae2a78a9980d6ca5f100e574cccd81f2
omrow <omrowiec@hotmail.com>
2016-05-09 17:05:34 +0200

    Delete for_you_ola.py

--- students_project/for_you_ola.py
+++
@@ -1,7 +0,0 @@
-from base1 import *
-
-# tu klep swoj kod....
-
-results = sentiment_classification(X, y, n_folds=4, classifier=LogisticRegression())
-print 'Accuracy: %s' % np.mean(results['acc'])
-print 'F1-measure: %s' % np.mean(results['f1'])
